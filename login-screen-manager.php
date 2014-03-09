9bee0ac20b57d36af26369a9bd1c53a6d6206c48
nazmul.hossain.nihal <nazmul.hossain.nihal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-09 18:57:30 +0000

    done
    git-svn-id: https://plugins.svn.wordpress.org/login-screen-manager/tags/2.0@872346 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login-screen-manager.php
+++ login-screen-manager.php
@@ -4,7 +4,7 @@ Plugin Name: Login Screen Manager
 Plugin URI: http://wordpress.org/extend/plugins/login-screen-manager
 Description: This plugin is for managing your login screen of WordPress site.
 Tags: login screen,login logo,wp-login.php,coding war,Nazmul Hossain Nihal,codingwar.com,logos,login screen manager
-Version: 1.1
+Version: 2.0
 Author:	Nazmul Hossain Nihal
 Author URI: http://www.codingwar.com
 License: GPLv2 or later
